a67185b24b5e37246f236b667f3c2881118135c2
BecooOn <adem.bekci.28@gmail.com>
2024-04-18 00:15:54 +0300

    civilization page with map method

--- src/helper/civilizations.js
+++ src/helper/civilizations.js
@@ -1,75 +1,91 @@
 const civilizations = [
   {
+    id: 1,
     name: "Ancient Egypt",
     foundation_year: "BC 3100",
     collapse_year: "AD 30",
     capital: "Memphis, Thebes, Cairo",
     important_sites: ["Pyramids", "Nile River", "Luxor Temple"],
     info: "Ancient Egypt has been a significant civilization throughout history. It has made important advancements in architecture, mathematics, medicine, and art.",
-    image: "https://pixabay.com/tr/illustrations/m%C4%B1s%C4%B1rl%C4%B1-tasar%C4%B1m-adam-kad%C4%B1n-rahip-1822015/",
+    image:
+      "https://pixabay.com/tr/illustrations/m%C4%B1s%C4%B1rl%C4%B1-tasar%C4%B1m-adam-kad%C4%B1n-rahip-1822015/",
   },
   {
+    id: 2,
     name: "Ancient Greece",
     foundation_year: "BC 800 ",
     collapse_year: "AD 146",
     capital: "Athens, Sparta",
     important_sites: ["Acropolis", "Olympia", "Delphi"],
     info: "Ancient Greece has hosted many important cultural and intellectual developments such as democracy, philosophy, theater, and the Olympic Games.",
-    image: "https://pixabay.com/tr/illustrations/adam-yunan-mitolojisi-mitoloji-din-8679501/",
+    image:
+      "https://pixabay.com/tr/illustrations/adam-yunan-mitolojisi-mitoloji-din-8679501/",
   },
   {
+    id: 3,
     name: "Roman Empire",
     foundation_year: "BC 753",
     collapse_year: "AD 476",
     capital: "Rome",
     important_sites: ["Colosseum", "Roman Forum", "Pantheon"],
     info: "The Roman Empire has controlled a significant portion of Europe, the Mediterranean, and the Middle East throughout history. It has left a significant impact with Roman law, architecture, and military techniques.",
-    image: "https://pixabay.com/tr/vectors/roma-kent-harita-hat-sanat%C4%B1-antik-5783047/",
+    image:
+      "https://pixabay.com/tr/vectors/roma-kent-harita-hat-sanat%C4%B1-antik-5783047/",
   },
   {
+    id: 4,
     name: "Sumerians",
     foundation_year: "BC 4000",
     collapse_year: "BC 2000",
     capital: "Ur, Uruk",
     important_sites: ["Ziggurats", "Ancient Mesopotamia"],
     info: "The Sumerians were an ancient civilization that established the first complex societies in Mesopotamia. They pioneered writing and made advancements in various fields of science.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Enthroned_King_of_Ur.jpg/1280px-Enthroned_King_of_Ur.jpg",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Enthroned_King_of_Ur.jpg/1280px-Enthroned_King_of_Ur.jpg",
   },
   {
+    id: 5,
     name: "Babylon",
     foundation_year: "BC 1894",
     collapse_year: "BC 539",
     capital: "Babylon",
     important_sites: ["Ishtar Gate", "Hanging Gardens", "Hammurabi's Code"],
     info: "The Babylonian Empire ruled Mesopotamia and was known for landmarks such as the Ishtar Gate and Hammurabi's Code.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Pieter_Bruegel_the_Elder_-_The_Tower_of_Babel_%28Vienna%29_-_Google_Art_Project_-_edited.jpg/1280px-Pieter_Bruegel_the_Elder_-_The_Tower_of_Babel_%28Vienna%29_-_Google_Art_Project_-_edited.jpg",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Pieter_Bruegel_the_Elder_-_The_Tower_of_Babel_%28Vienna%29_-_Google_Art_Project_-_edited.jpg/1280px-Pieter_Bruegel_the_Elder_-_The_Tower_of_Babel_%28Vienna%29_-_Google_Art_Project_-_edited.jpg",
   },
   {
+    id: 6,
     name: "Scythians",
     foundation_year: "BC 800",
     collapse_year: "AD 300",
     capital: "Scythia",
     important_sites: ["Pazyryk Burial Mound", "Altai Mountains"],
     info: "The Scythians were a nomadic people who ruled various regions of Eurasia. They are known for their goldwork and warrior culture.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/2/2f/PazyrikHorseman.JPG",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/2/2f/PazyrikHorseman.JPG",
   },
   {
+    id: 7,
     name: "Inca Empire",
     foundation_year: "AD 1200",
     collapse_year: "AD 1572",
     capital: "Cuzco",
     important_sites: ["Machu Picchu", "Saksaywaman", "Qorikancha"],
     info: "The Incas were a great civilization in the Andes Mountains of South America. They are known for their architecture and agricultural techniques.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Ingapirca01.jpg/1280px-Ingapirca01.jpg",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Ingapirca01.jpg/1280px-Ingapirca01.jpg",
   },
   {
+    id: 8,
     name: "Maya Civilization",
     foundation_year: "BC 2000",
     collapse_year: "AD 1697",
     capital: "Tikal, Chichen Itza",
     important_sites: ["Chichen Itza Pyramid", "Palenque", "Tikal"],
     info: "The Maya civilization flourished in the rainforests of Central America. They made significant advancements in mathematics, astronomy, and writing.",
-    image: "https://pixabay.com/tr/photos/meksika-chichen-itz%C3%A1-piramit-mimari-3774303/",
+    image:
+      "https://pixabay.com/tr/photos/meksika-chichen-itz%C3%A1-piramit-mimari-3774303/",
   },
 ];
 
--- src/pages/civilizations/Civilizations.jsx
+++ src/pages/civilizations/Civilizations.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./Civilizations.scss";
+import civilizations from "../../helper/civilizations";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-const Projects = () => {
+const Civilizations = () => {
+  console.log(civilizations);
   return (
-    <div>Projects</div>
-  )
-}
+    <div>
+      {civilizations.map((civilization) => (
+        <Card key={civilization.id} style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={civilization.image}
+            width={"200px"}
+            height={"250px"}
+          />
+          <Card.Body>
+            <Card.Title>{civilization.name}</Card.Title>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
+};
 
-export default Projects
+export default Civilizations;
